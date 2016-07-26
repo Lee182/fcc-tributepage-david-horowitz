8d0f963bb8f3b41e17a8661596de6dfbac843e13
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2016-07-26 12:57:29 +0100

    saving project to github

---
+++ pdfcopy.js
@@ -0,0 +1,22 @@
+sta = `
+Horowitz   obtained   press   credentials   at   the
+newly  formed  United  Nations  under  the  auspices  of
+World  Union  Press,  which  he  had  formed  as  an  arm
+of  UIWU.  He  was  present  at  the  opening  sessions  in
+San   Francisco   and   at   Lake   Success,   NY.   He
+maintained    his    UN    office    for    over    50    years,
+publishing  a  weekly  syndicated  column,  which  at  its
+peak,  appeared  in  approximately  40  Anglo-Jewish
+newspapers.
+Horowitz     was     an
+outspoken      Zionist
+and      represented
+Israel     and     Jewish
+interests   at   the   UN
+during  his  long  and
+distinguished career.
+`
+
+function tri(str) {
+  return str.split(/ +/).join(' ').split('\n').join('').trim()
+}
